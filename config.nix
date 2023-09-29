356d4232db3965c9dd2d9a3c2ab011e214e02710
Kevin Bernfeld <kcbernfeld@gmail.com>
2023-09-28 21:12:58 -0400

    postgres

--- config.nix
+++ config.nix
@@ -3,17 +3,11 @@
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages = [
-    pkgs.postgresql_15
-  ];
+  environment.systemPackages = [ ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   environment.darwinConfig = "$HOME/dotfiles/config.nix";
-
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
@@ -42,5 +36,25 @@
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
+  };
+
+  services = {
+    # Auto upgrade nix package and the daemon service.
+    nix-daemon = {
+      enable = true;
+      # nix.package = pkgs.nix;
+    };
+
+    postgresql = {
+      enable = true;
+      package = pkgs.postgresql_15;
+      enableTCPIP = true;
+      authentication = pkgs.lib.mkOverride 10 ''
+        # type  database  DBuser  origin-address  auth-method
+        local   all       all                     trust
+        host    all       all     127.0.0.1/32    trust
+        host    all       all     ::1/128         trust
+      '';
+    };
   };
 }
