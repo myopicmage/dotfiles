be6822e2ce404a18073eddb9e4477ef65f3c6f85
Kevin Bernfeld <kcbernfeld@gmail.com>
2023-11-06 13:07:36 -0500

    no more vs code

--- config.nix
+++ config.nix
@@ -7,15 +7,6 @@
     gnupg
     iterm2
     slack
-    ((vscode.override { isInsiders = true; }).overrideAttrs
-      (oldAttrs: rec {
-        src = (builtins.fetchTarball {
-          url = "https://code.visualstudio.com/sha/download?build=insider&os=darwin-universal";
-          sha256 = "1kghyg9hx2nzn8la67052l17xh1rxh6y7k3pw7id53i4s988l6dy";
-        });
-        version = "latest";
-        buildInputs = oldAttrs.buildInputs ++ [ krb5 ];
-      }))
   ];
 
   # Use a custom configuration.nix location.
@@ -64,6 +55,8 @@
     enable = true;
     onActivation.autoUpdate = true;
     brews = [
+      "flyctl"
+      "swiftlint"
       "vapor"
     ];
     casks = [
